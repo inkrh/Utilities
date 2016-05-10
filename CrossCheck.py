aaaf0d925c3b02284a1b57e6efc3c58214abd15f
Robert Henden <robert@inkrh.com>
2016-05-10 17:04:07 -0500

    Python SQL Comparison

---
+++ CrossCheck.py
@@ -0,0 +1,67 @@
+import sqlite3
+import csv
+
+
+#plenty of room for improvement here, but these are the basics
+#bulk borrowed from https://pysqlite.readthedocs.io/en/latest/sqlite3.html
+
+def compare(csvOut, dbase1, dbase2, command):
+    if type(dbase1) != str or type(dbase2) != str or type(command) != str:
+        print "Input type error"
+        return None
+
+ 
+    
+    con = sqlite3.connect(dbase1)
+    con2 = sqlite3.connect(dbase2)
+    
+    con.isolation_level = None
+    con2.isolation_level = None
+    
+    cur = con.cursor()
+    cur2 = con2.cursor()
+    
+    if sqlite3.complete_statement(command):
+        try:
+            command = command.strip()
+            cur.execute(command)
+            cur2.execute(command)
+            resultOne = cur.fetchall()
+            resultTwo = cur2.fetchall()
+            output = [resultOne,resultTwo,resultOne==resultTwo]
+            csvOut.writerow(output)
+
+            
+            if resultOne == resultTwo:
+                print "Matched"
+                
+            else:
+                print "Not matched"
+                
+        except sqlite3.Error, e:
+            print "An error occurred:", e.args[0]
+            con.close()
+            con2.close()
+            return
+        
+    else:
+        print "Invalid SQL command"
+        
+    con.close()
+    con2.close()
+    return
+
+
+def iterate(filename, dbase1, dbase2, commandList):
+    fileInstance = open(filename, 'wb')
+    csvHandler = csv.writer(fileInstance)
+    
+    if type(commandList) == str:
+        #single command compare
+        compare(csvHandler, dbase1, dbase2, commandList)
+    elif type(commandList) == list:
+        #list of commands compare
+        for c in commandList:
+            compare(csvHandler, dbase1, dbase2, c)
+
+    fileInstance.close()
---
+++ WP8.1_ISETool.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files (x86)\Microsoft SDKs\Windows Phone\v8.0\Tools\IsolatedStorageExplorerTool\
+isetool ts de %1 %2
+pause
