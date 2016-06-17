4176da4544a0f7037f7419805586773e514ed10d
Robert Henden <robert@inkrh.com>
2016-06-16 20:10:43 -0500

    Convert a file to midi note - added time element

--- FileToMidi.py
+++ FileToMidi.py
@@ -1,4 +1,5 @@
 import mido
+import time
 
 def Run(fi):
     with open(fi, "rb") as imageFile:
@@ -7,15 +8,18 @@ def Run(fi):
     
     old = 0
     o = mido.open_output()
-    for i in range(0,len(b)-1):
+    for i in range(0,len(b)-2):
         sn = b[i]
         sv = b[i+1]
+        t = b[i+2]
+        
         if sn > 127:
             sn = sn/2
         if sv > 127:
             sv =sv/2
 
         o.send(mido.Message('note_on', note=sn, velocity=sv))
+        time.sleep(t/100)
         o.send(mido.Message('note_off', note=old))
 
         old = sn
