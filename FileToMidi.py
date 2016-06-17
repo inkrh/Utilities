a40d9bcb6053c499386e888f9cb2ddbe4011fdcb
Robert Henden <robert@inkrh.com>
2016-06-17 10:37:43 -0500

    Microgranny control

--- FileToMidi.py
+++ FileToMidi.py
@@ -1,7 +1,7 @@
 import mido
 import time
 
-def Run(fi, minnote=21,maxnote=108, minvelocity=21, timingdivisor=127, shortestnoteon=0.00390625, step=3, mono=True):
+def Run(fi, minnote=21, maxnote=108, minvelocity=64, timingdivisor=127, shortestnoteon=0.00390625, step=3, mono=True, microgranny=True):
 
     if minnote < 0:
         print("Negative minimum note")
@@ -27,19 +27,22 @@ def Run(fi, minnote=21,maxnote=108, minvelocity=21, timingdivisor=127, shortestn
     
     o = mido.open_output()
     for i in range(0,len(b)-2, step):
-        sn = b[i]
-        sv = b[i+1]
+        sn = b[i]%127
+        sv = b[i+1]%127
         t = max(shortestnoteon,b[i+2]/timingdivisor)
 
-        ##there is a far better way to do this, but I need sleep
-        
-        ##wraparound 0<sn<255 to 0<sn<127 should make this wraparound at min/max notes
-        if sn > 127:
-            sn = sn/2
-            
-        if sv > 127:
-            sv =sv/2
+        if microgranny:
 
+            ##send sample change
+            if sn in range(0,6):
+                o.send(mido.Message('note_on', note = sn))
+
+            ##send command change (sampleRate,crush,attack,release,grainSize,shiftSpeed,start,end)
+            if sn in range(102,111):
+                o.send(mido.Message('control_change',control=sn,value=sv))
+
+
+        sn = sn%maxnote
         
         sv = max(sv,minvelocity)
         
