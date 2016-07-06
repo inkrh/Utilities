c7d5aa35002eb34cddaa9da1d70bcbf3c5885da9
Robert Henden <robert@inkrh.com>
2016-07-05 20:02:18 -0500

    Minor refactoring

--- KaossilatorFileToMidi.py
+++ KaossilatorFileToMidi.py
@@ -1,7 +1,7 @@
 import mido
 import time
 
-def Run(fi, minnote=21, maxnote=108, minvelocity=100, maxvelocity=115, timingdivisor=127, shortestnoteon=0.00390625, step=3, loop=False):
+def Run(fi, minnote=21, maxnote=108, minypad=0, maxypad=127, timingdivisor=127, shortestnoteon=0.00390625, step=3, loop=False):
 
     if minnote < 0:
         print("Negative minimum note")
@@ -26,20 +26,20 @@ def Run(fi, minnote=21, maxnote=108, minvelocity=100, maxvelocity=115, timingdiv
     with mido.open_output() as o:
         if(loop):
             while True:
-                Play(o, b,minnote,maxnote, minvelocity, maxvelocity, timingdivisor,shortestnoteon,step)
+                Play(o, b,minnote,maxnote, minypad, maxypad, timingdivisor,shortestnoteon,step)
                 
         else:
-            Play(o, b,minnote,maxnote, minvelocity, maxvelocity, timingdivisor,shortestnoteon,step)
+            Play(o, b,minnote,maxnote, minypad, maxypad, timingdivisor,shortestnoteon,step)
 
 
-def Play(o, b,minnote,maxnote, minvelocity, maxvelocity, timingdivisor,shortestnoteon,step):
+def Play(o, b,minnote,maxnote, minypad, maxypad, timingdivisor,shortestnoteon,step):
     for i in range(0,len(b)-2, step):
     ##because I wanted to just grab a small subset for a loop
     ##for i in range(23037,23046):
         
-            ##note i, velocity i+1, time i+2
-            sn = (b[i]+minnote)%127
-            sv = (b[i+1]+minvelocity)%127
+            ##note i, ypad i+1, time i+2
+            sx = (b[i]+minnote)%127
+            sy = (b[i+1]+minypad)%127
             
             ##is a better way of handling the timing but will fix it when I need to
             ##or when I feel like it, whichever happens first
@@ -51,34 +51,23 @@ def Play(o, b,minnote,maxnote, minvelocity, maxvelocity, timingdivisor,shortestn
                 t = max(shortestnoteon,b[i+2]/timingdivisor)
 
 
-##            ##set min and max velocity
-            velocityrange = maxvelocity-minvelocity
-            sv = (sv%velocityrange)+minvelocity
-
+##            ##set min and max ypad
+            ypadrange = maxypad-minypad
+            sy = (sy%ypadrange)+minypad
+            noterange = maxnote-minnote
+            sx = (sx%noterange)+minnote
+            ##pad pressed
             o.send(mido.Message("control_change",channel=0,control=92,value=127))
-            
-            o.send(mido.Message("control_change",channel=0,control=12,value=sn,time=0))
-            o.send(mido.Message("control_change",channel=0,control=13,value=sv,time=0))
-            print("Byte set " + str(i) + " : sn " + str(sn) + " : sv " + str(sv) + " : t " + str(t))
+            ##x/y position of press
+            o.send(mido.Message("control_change",channel=0,control=12,value=sx,time=0))
+            o.send(mido.Message("control_change",channel=0,control=13,value=sy,time=0))
+            print("Byte set " + str(i) + " : sx " + str(sx) + " : sy " + str(sy) + " : t " + str(t))
 
             time.sleep(t)
+            ##pad released
             o.send(mido.Message("control_change",channel=0,control=92,value=0))
 
-##
-##            ##send output, send as note off if below lowest (forcelowest)
-##            if sn < minnote:
-##                o.send(mido.Message('note_off', note=sn, time=t))
-##            else:
-##                o.send(mido.Message('note_on', note=sn, velocity=sv, time=t))
-##
-##            ##one note at a time, sleep between messages
-##            if mono:
-##                time.sleep(t)
-##                ##cover not responding to time in mido.Message
-##                o.send(mido.Message('note_off', note=sn))
-
 ##examples of settings, let the randomness begin
-##depends on file input but with image file of ~93kb step of 384 and divisor of 63.75 will be ~8.5 minutes
                 
 ##good settings for Kaossilator Pro Plus drum loops
 def ProcessFile(fi):
@@ -86,6 +75,6 @@ def ProcessFile(fi):
 
 def ProcessFileLoop(fi):
     Run(fi,minnote=48,maxnote=60,step=192,timingdivisor=33,loop=True)
-
+##good settings for Kaossilator Pro Plus non-looped voices
 def IndividualProcessFileLoop(fi):
-    Run(fi,minnote=48,maxnote=60,step=192,timingdivisor=127,loop=True)
+    Run(fi,minnote=0,maxnote=127,step=3,loop=True)
