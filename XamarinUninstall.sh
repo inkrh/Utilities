5a91cb792f92fc712c55ff4b3fb70021f2a01aae
Robert Henden <roberthenden@Roberts-MacBook-Pro.local>
2016-09-21 11:04:13 -0500

    Xamarin Uninstaller

---
+++ XamarinUninstall.sh
@@ -0,0 +1,17 @@
+sudo rm -rf "/Applications/Xamarin Studio.app"
+rm -rf ~/Library/Caches/XamarinStudio-*
+rm -rf ~/Library/Preferences/XamarinStudio-*
+rm -rf ~/Library/Logs/XamarinStudio-*
+rm -rf ~/Library/XamarinStudio-*
+rm -rf ~/Library/MonoTouch
+sudo rm -rf /Library/Frameworks/Xamarin.iOS.framework
+sudo rm -rf /Developer/MonoTouch
+sudo pkgutil --forget com.xamarin.monotouch.pkg
+sudo pkgutil --forget com.xamarin.xamarin-ios-build-host.pkg
+sudo rm -rf /Library/Frameworks/Xamarin.Mac.framework
+rm -rf ~/Library/Xamarin.Mac
+sudo rm -rf /Developer/MonoDroid
+rm -rf ~/Library/MonoAndroid
+sudo pkgutil --forget com.xamarin.android.pkg
+sudo rm -rf /Library/Frameworks/Xamarin.Android.framework
+
