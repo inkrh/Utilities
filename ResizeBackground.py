ca4c059020366e27d38c4a199f0135fc22bfb3ee
Robert Henden <roberthenden@Roberts-MacBook-Pro.local>
2016-09-22 14:28:23 -0500

    image name extensions

--- ResizeBackground.py
+++ ResizeBackground.py
@@ -2,7 +2,16 @@ from PIL import Image
 from resizeimage import resizeimage
 
 
-sizes= [(640,1136),(750,1334),(1242,2208),(1536,2048),(2048,2732)]
+sizes= [(640,960),(640,1136),(750,1334),(1242,2208),(768,1024),(2048,2732)]
+
+
+
+names = ["@2x~iphone","-568@2x~iphone","-667@2x~iphone","-736@3x~iphone","~ipad","-1366@2x~ipad"]
+
+def pl(x,y):
+    if x > y:
+        return "-Landscape"
+    return "-Portrait"
 
 
 def resize(filename, image, x,y):
@@ -13,11 +22,13 @@ def resize(filename, image, x,y):
 
 def Process(file):
     i = Image.open(file)
+    c = 0
     for x,y in sizes:
         #portrait
-        resize(file.replace('.png','p'+str(x)+'_'+str(y)+'.png'),i,x,y)
+        resize(file.replace('.png',pl(x,y)+names[c]+'.png'),i,x,y)
         #landscape
-        resize(file.replace('.png','l'+str(y)+'_'+str(x)+'.png'),i,y,x)
+        resize(file.replace('.png',pl(y,x)+names[c]+'.png'),i,y,x)
+        c += 1
         
         
     
