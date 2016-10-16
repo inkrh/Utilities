a5f9d417fe8eca6e2271a441340e19283b21c40f
Robert Henden <roberthenden@Roberts-MacBook-Pro.local>
2016-10-15 22:58:34 -0500

    Housekeeping?

--- General/houseKeep.sh
+++ General/houseKeep.sh
@@ -1,2 +1,6 @@
 sudo periodic daily weekly monthly
+find ~/Projects -iname "._*" -exec rm -rf {} \;
+find ~/Documents -iname "._*" -exec rm -rf {} \;
+find ~/Projects -iname ".DS*" -exec rm -rf {} \;
+find ~/Documents -iname ".DS*" -exec rm -rf {} \;
 
