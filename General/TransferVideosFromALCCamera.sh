24e161e2724502250e3ab7c095083b9e2507a27f
Robert Henden <roberthenden@Roberts-MacBook-Pro.local>
2016-10-19 04:00:45 -0500

    Helper to transfer all video files from an ALC brand camera without having to use their godawful apps

---
+++ General/TransferVideosFromALCCamera.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "$0"
+echo ALC address "$1"
+echo ftp user "$2"
+echo ftp address "$4"
+echo ftp folder "$5"
+spawn telnet $1
+expect "login:"
+send "root"
+expect "Password:"
+send ""
+cd /mnt/sd/VideoPlayback;
+for f in *.avi;
+do 
+  ftpput -v -u "$2" -p "$3" "$4" "$5""$f" "$f"
+done
+
+##use ./TransferVideosFromALCCamera.sh ALC user pass ftpserver destfolder
+##use sudo -s launchctl load -w /System/Library/LaunchDaemons/ftp.plist
+##to start an ftp server on localhost
+
+##e.g. ./TransferVideosFromALCCamera.sh 10.0.1.20 ftpUser ftpPass 10.0.1.1 /Volumes/Storage/ 
