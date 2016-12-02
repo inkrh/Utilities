2cb3260aafff5fea0d75bcffecc312a7854c4b81
Robert Henden <roberthenden@Roberts-MacBook-Pro.local>
2016-12-02 16:03:45 -0600

    Audio glitcher

--- Audio/Glitcher.py
+++ Audio/Glitcher.py
@@ -9,23 +9,26 @@ def Process(source,codec =""):
 
     songarray = []
 
-    step = len(song)/1000
+    step = len(song)/100
     for i in range(0,len(song),step):
         songarray = songarray + [song[i:i+step]]
 
     out = songarray[0]
     
     for i in range(1,len(songarray)):
-        if random.randint(1,20)<12:
-            out = out + songarray[random.randint(0,len(songarray)-1)]
-        if random.randint(2,42)<30:
-            out = out + songarray[i].reverse()
-            
-        out = out + songarray[i]
-
-        if random.randint(3,42)<40:
+        if random.randint(1,5)<3:
+            if random.randint(1,20)<8:
+                if i<len(songarray)-2 and i>1:
+                    out = out + songarray[i+random.randint(0,2)]
+            if random.randint(2,42)<16:
+                out = out + songarray[i].reverse()
+                
             out = out + songarray[i]
 
+            if random.randint(3,42)<32:
+                out = out + songarray[i]
+        else:
+            out = out + songarray[i]
 
     out.export("glitched.wav", format="wav")
     
