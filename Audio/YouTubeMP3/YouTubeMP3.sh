42479616b539428a084be8109e2d58bc6211e8e3
Robert Henden <roberthenden@Roberts-MacBook-Pro.local>
2017-08-31 10:28:03 -0500

    YouTube MP3 Helper

---
+++ Audio/YouTubeMP3/YouTubeMP3.sh
@@ -0,0 +1,15 @@
+if [ -z "$1" ]
+  then
+    echo "Missing URL(s)"
+    echo "Syntax:"
+    echo "YouTubeMP3.sh <YouTube URL>" 
+fi
+
+echo "Grabbing YouTube URLs and saving them as mp3s"
+
+for var in "$@"
+  do
+    echo "STARTING $var"
+    youtube-dl --extract-audio --audio-format mp3 "$var"
+    echo "$var DONE"
+  done
