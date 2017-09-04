6a4db145e573b2c0137260b0f4db5c40c8787db0
Robert Henden <roberthenden@Roberts-MacBook-Pro.local>
2017-09-03 19:24:56 -0500

    Forgot to handle conditional in bash script

--- Audio/YouTubeMP3/YouTubeMP3.sh
+++ Audio/YouTubeMP3/YouTubeMP3.sh
@@ -3,13 +3,12 @@ if [ -z "$1" ]
     echo "Missing URL(s)"
     echo "Syntax:"
     echo "YouTubeMP3.sh <YouTube URL>" 
+  else
+    echo "Grabbing YouTube URLs and saving them as mp3s"
+    for var in "$@"
+      do
+        echo "STARTING $var"
+        youtube-dl --extract-audio --audio-format mp3 "$var"
+        echo "$var DONE"
+    done
 fi
-
-echo "Grabbing YouTube URLs and saving them as mp3s"
-
-for var in "$@"
-  do
-    echo "STARTING $var"
-    youtube-dl --extract-audio --audio-format mp3 "$var"
-    echo "$var DONE"
-  done
